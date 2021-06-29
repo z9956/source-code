62fd42ee4ba9040d09c64ebfde09c70a048ac7ca
z9956 <zhz995617@gmail.com>
2021-06-29 15:59:46 +0800

    style: style

--- src/examples.js
+++ src/examples.js
@@ -3,53 +3,53 @@ import Eventbus from './eventbus';
 const $root = document.getElementById('root');
 const eventbus = new Eventbus();
 
-function createElementAndBindEvent({tagName, text, event}) {
-  const element = document.createElement(tagName);
-  const content = document.createTextNode(text);
+function createElementAndBindEvent({ tagName, text, event }) {
+	const element = document.createElement(tagName);
+	const content = document.createTextNode(text);
 
-  element.appendChild(content);
+	element.appendChild(content);
 
-  element.onclick = () => {
-    event && event();
-  };
+	element.onclick = () => {
+		event && event();
+	};
 
-  return element;
+	return element;
 }
 
 function eventbusMain() {
-  eventbus.on('a', (data) => {
-    console.log({data});
-  });
+	eventbus.on('a', (data) => {
+		console.log({ data });
+	});
 
-  eventbus.once('b', () => {
-    console.log('bbb');
-  });
+	eventbus.once('b', () => {
+		console.log('bbb');
+	});
 
-  const eventList = [
-    {
-      tagName: 'button',
-      text: 'a event',
-      event: () => {
-        eventbus.emit('a', {a: 1});
-      },
-    },
-    {
-      tagName: 'button',
-      text: 'b event',
-      event: () => {
-        eventbus.emit('b', 'b');
-      },
-    },
-  ];
+	const eventList = [
+		{
+			tagName: 'button',
+			text: 'a event',
+			event: () => {
+				eventbus.emit('a', { a: 1 });
+			},
+		},
+		{
+			tagName: 'button',
+			text: 'b event',
+			event: () => {
+				eventbus.emit('b', 'b');
+			},
+		},
+	];
 
-  eventList.forEach((event) => {
-    const element = createElementAndBindEvent(event);
-    $root.appendChild(element);
-  });
+	eventList.forEach((event) => {
+		const element = createElementAndBindEvent(event);
+		$root.appendChild(element);
+	});
 }
 
 function main() {
-  eventbusMain();
+	eventbusMain();
 }
 
 export default main;
